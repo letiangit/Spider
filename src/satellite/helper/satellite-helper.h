cdbb9e56685866500515b464c1ac44026699bb86
letiangit <letian.wlh@gmail.com>
2017-09-05 14:53:30 +0200

    create satellite module skeleton

---
+++ src/satellite/helper/satellite-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef SATELLITE_HELPER_H
+#define SATELLITE_HELPER_H
+
+#include "ns3/satellite.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* SATELLITE_HELPER_H */
+
---
+++ src/satellite/model/satellite.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef SATELLITE_H
+#define SATELLITE_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* SATELLITE_H */
+
